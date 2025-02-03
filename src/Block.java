07c72d5aebf00f949112df7adee6aa74def1ab0a
sushanthacharya2003 <acharyasushanth37@gmail.com>
2025-02-03 18:09:33 +0530

    block created

--- src/Block.java
+++ src/Block.java
@@ -5,15 +5,38 @@ public class Block {
     public String previousHash;
     public String data;
     private long timeStamp;
-    public Block(String data,String previousHash){
-        this.data=data;
-        this.previousHash=previousHash;
-        this.timeStamp=new Date().getTime();
+    private int nonce;
+
+    public Block(String data, String previousHash) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
         this.hash = calculateHash();
     }
-    public String calculateHash(){
-        String calculatedhash=StringUtil.applysha256(previousHash+Long.toString(timeStamp)+data);
+
+    public String calculateHash() {
+        String calculatedhash = StringUtil.applysha256(previousHash + Long.toString(timeStamp) + data);
         return calculatedhash;
     }
 
+    public static Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+        for (int i = 1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+            //compare registered hash and calculated hash:
+            if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+            //compare previous hash and registered previous hash
+            if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
+                System.out.println("Previous Hashes not equal");
+                return false;
+
+            }
+    }
+    return true;
+    }
 }
